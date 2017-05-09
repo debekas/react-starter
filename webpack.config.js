e8ffe05400d4a3860649901814ac91b084734b5a
Denis Bekasow <Denis Bekasow>
2017-05-09 18:33:43 +0200

    base config

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+
+module.exports = {
+  context: resolve(__dirname, 'src'),
+  entry: {
+    bundle: [
+      'react-hot-loader/patch',
+      './index.js',
+    ],
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    hotOnly: true,
+    stats: 'errors-only',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        exclude: /node_modules/,
+        use: 'url-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+};
